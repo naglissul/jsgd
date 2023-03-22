a42f4f5b6ec82db7baf2f93aea970ed60c1ef9cb
Naglis <naglis1134@gmail.com>
2023-03-22 14:00:07 +0100

    add auto player remove, bullets (lagging...)

--- public/script.js
+++ public/script.js
@@ -7,7 +7,9 @@ canvas.height = innerHeight
 let playerRef
 let playerId
 const allPlayersRef = firebase.database().ref('players')
-let players = []
+const allBulletsRef = firebase.database().ref('bullets')
+let players = {}
+let bullets = {}
 let velY = 0
 let velX = 0
 
@@ -37,6 +39,7 @@ firebase
         //...
         console.log(errorCode, errorMsg)
     })
+
 //----------------------------------------------------
 function jump() {
     velY = -20
@@ -45,32 +48,26 @@ function jump() {
 //LISTENERS
 function initListeners() {
     allPlayersRef.on('value', (snapshot) => {
-        c.clearRect(0, 0, canvas.width, canvas.height)
-
-        c.font = '30px Arial'
-        c.fillStyle = 'red'
-        c.fillText('Left, Right, Space, A', 500, 100)
-        c.font = '10px Arial'
-        //draw all players
         players = snapshot.val() || {}
-        Object.keys(players).forEach((key) => {
-            c.fillStyle = players[key].color
-            c.fillRect(players[key].x, players[key].y, 30, 30)
-            c.fillStyle = 'black'
-            c.textAlign = 'center'
-            c.fillText(
-                players[key].name,
-                players[key].x + 15,
-                players[key].y - 3
-            )
-        })
     })
 
     allPlayersRef.on('child_added', (snapshot) => {
         players = snapshot.val() || {}
     })
 
-    new KeyListener('Space', () => jump())
+    allBulletsRef.on('value', (snapshot) => {
+        bullets = snapshot.val() || {}
+    })
+
+    allBulletsRef.on('child_added', (snapshot) => {
+        bullets = snapshot.val() || {}
+    })
+
+    new KeyListener(
+        'Space',
+        () => jump(),
+        () => {}
+    )
     new KeyListener(
         'ArrowLeft',
         () => (velX = -15),
@@ -84,42 +81,83 @@ function initListeners() {
     new KeyListener(
         'KeyA',
         () =>
-            (players = {
-                ...players,
+            (bullets = {
+                ...bullets,
                 [playerId + parseInt(Math.random() * 100)]: {
-                    x: players[playerId].x,
-                    y: players[playerId].y,
-                    color: 'black',
-                    name: '',
+                    x: players[playerId].x + 30,
+                    y: players[playerId].y + 10,
                 },
             }),
         () => {}
     )
+
+    addEventListener('visibilitychange', () => {
+        playerRef.remove()
+        alert("Dude, don't change the visibility...")
+    })
 }
 
 //PLAYER ME------------------------------------------
 function animate() {
     requestAnimationFrame(animate)
-    velY += 1
-    let x = players[playerId].x
-    let y = players[playerId].y
-    x += velX
-    y += velY
+    if (players[playerId]) {
+        velY += 1
+        let x = players[playerId].x
+        let y = players[playerId].y
+        x += velX
+        y += velY
 
-    if (y > innerHeight - 30) {
-        velY = -velY + 5
-        y = innerHeight - 30
-    }
-    if (x > innerWidth - 30) {
-        x = innerWidth - 30
-    }
-    if (x < 0) {
-        x = 0
-    }
+        if (y > canvas.height - 30) {
+            velY = -velY + 5
+            y = canvas.height - 30
+        }
+        if (x > canvas.width - 30) {
+            x = canvas.width - 30
+        }
+        if (x < 0) {
+            x = 0
+        }
 
-    players[playerId].x = x
-    players[playerId].y = y
+        Object.keys(bullets)
+            .filter((id) => id.startsWith(playerId))
+            .forEach((key) => {
+                bullets[key].x += 5
+                if (bullets[key].x > canvas.width) {
+                    delete bullets[key]
+                }
+            })
+        allBulletsRef.set(bullets)
 
-    allPlayersRef.set(players)
+        //RENDER
+        c.clearRect(0, 0, canvas.width, canvas.height)
+
+        //DRAW TEXT
+        c.font = '30px Arial'
+        c.fillStyle = 'red'
+        c.fillText('Left, Right, Space, A', 500, 100)
+        c.font = '10px Arial'
+
+        //DRAW PLAYERS
+        Object.keys(players).forEach((key) => {
+            c.fillStyle = players[key].color
+            c.fillRect(players[key].x, players[key].y, 30, 30)
+            c.fillStyle = 'black'
+            c.textAlign = 'center'
+            c.fillText(
+                players[key].name,
+                players[key].x + 15,
+                players[key].y - 3
+            )
+        })
+
+        //DRAW BULLETS
+        c.fillStyle = 'black'
+        Object.keys(bullets).forEach((key) => {
+            c.fillRect(bullets[key].x, bullets[key].y, 10, 10)
+        })
+
+        playerRef.update({ x: x, y: y })
+    }
 }
+
 animate()
