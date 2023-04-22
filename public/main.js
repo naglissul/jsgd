8044235f7a083d59b48f5f5ed992048cfd0dfc3a
Naglis <naglis1134@gmail.com>
2023-04-22 15:08:39 +0200

    name autocomplete

--- public/main.js
+++ public/main.js
@@ -1,12 +1,29 @@
+;(function () {
+    const input = document.querySelector('input')
+    const lastValue = localStorage.getItem('name')
+    if (lastValue) {
+        input.value = lastValue
+    }
+
+    input.addEventListener('input', (event) => {
+        const value = event.target.value
+        localStorage.setItem('name', value)
+    })
+
+    input.addEventListener('focus', () => {
+        input.selectionStart = input.value.length
+        input.selectionEnd = input.value.length
+    })
+})()
+
 function nameGiven(name) {
     document.querySelector('input').remove()
     const canvas = document.createElement('canvas')
     document.body.appendChild(canvas)
     new Game(canvas, name)
 }
-//Make an order in joining/leaving players. names
 //make work and not lag for mac and mobile
+//name autocomplete
+//congrats something more
 
-//Original picture appear
-//good/bad and count
 //Continue with the original idea
